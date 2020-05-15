340ef6730b3c7026bf18bd003c957340bc51851c
Alfredo Benassi <alfrebenassi@gmail.com>
2020-05-15 18:47:25 -0300

    Created Folder Structure

---
+++ projects/ngx-table/src/lib/ngx-table.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxTableComponent } from './ngx-table.component';
+
+
+
+@NgModule({
+  declarations: [NgxTableComponent],
+  imports: [
+  ],
+  exports: [NgxTableComponent]
+})
+export class NgxTableModule { }
